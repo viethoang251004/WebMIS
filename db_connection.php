2f36acffa944e698d8e6d44aee888c6db3754ad6
tan06022004 <522h0012@student.tdtu.edu.vn>
2024-11-20 22:06:23 +0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -12,5 +12,4 @@ $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully!";
 ?>
